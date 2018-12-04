5fcf8074326975e10c32b1dec46f6e0abd4ee21d
MasonProtter <mason.protter@gmail.com>
2018-12-04 15:56:56 -0700

    setup

---
+++ src/RewriteRepl.jl
@@ -0,0 +1,29 @@
+# [[file:~/Documents/Julia/RewriteRepl/README.org::*Source%20Code][Source Code:1]]
+module RewriteRepl
+
+using ReplMaker, Rewrite
+
+Rewrite.normalize(t::Term, tup::Tuple) = normalize(t, tup...)
+
+if isdefined(Base, :active_repl)
+     initrepl(
+         prompt_text  = "@term> ",
+         prompt_color = :cyan, 
+         start_key    = '=', 
+         mode_name    = "Rewrite_mode",
+     ) do s
+         exs = [Meta.parse(i) for i in split(s, "with")]
+         if length(exs) == 1
+             :(normalize((@term $(exs[1]))))
+         elseif length(exs) == 2
+             :(normalize((@term $(exs[1])), $(exs[2])))
+         else
+             throw("Only one `with` statement allowed.")
+         end
+     end
+ end
+
+export normalize
+
+end
+# Source Code:1 ends here
